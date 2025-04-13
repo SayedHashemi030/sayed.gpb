7e671e2626693d29207498ff7f9872b8b84b33d0
Sayed Hashemi <your.sayedhashemi030@gmail.com>
2025-04-14 01:15:10 +0200

    pdate form and design

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
 
     // گرفتن اطلاعات از فرم
     var name = document.getElementById('name').value;
-    var age = document.getElementById('age').value;
+    var city = document.getElementById('city').value;
     var phone = document.getElementById('phone').value;
-    var hobbies = document.getElementById('hobbies').value;
-    var eyeColor = document.getElementById('eyeColor').value;
+    var address = document.getElementById('address').value;
+    var dob = document.getElementById('dob').value;
+    var gender = document.getElementById('gender').value;
     var status = document.getElementById('status').value;
+    var nationality = document.getElementById('nationality').value;
 
     // نمایش اطلاعات وارد شده
     document.getElementById('result').style.display = 'block';
     document.getElementById('greeting').textContent = 'Vielen Dank, ' + name + '!';
-    document.getElementById('resultName').textContent = 'Name: ' + name;
-    document.getElementById('resultAge').textContent = 'Alter: ' + age;
+    document.getElementById('resultName').textContent = 'Vollständiger Name: ' + name;
+    document.getElementById('resultCity').textContent = 'Stadt: ' + city;
     document.getElementById('resultPhone').textContent = 'Telefonnummer: ' + phone;
-    document.getElementById('resultHobbies').textContent = 'Hobbys: ' + hobbies;
-    document.getElementById('resultEyeColor').textContent = 'Augenfarbe: ' + eyeColor;
+    document.getElementById('resultAddress').textContent = 'Adresse: ' + address;
+    document.getElementById('resultDob').textContent = 'Geburtsdatum: ' + dob;
+    document.getElementById('resultGender').textContent = 'Geschlecht: ' + gender;
     document.getElementById('resultStatus').textContent = 'Familienstand: ' + status;
+    document.getElementById('resultNationality').textContent = 'Nationalität: ' + nationality;
 
-    // فرم ارسال می‌شود
+    // ارسال فرم
     event.target.submit();
 });
